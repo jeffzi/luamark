3e19b4569e981b7981ab42468396deed6bca37f5
Jean-Francois Zinque <16199817+jeffzi@users.noreply.github.com>
2024-01-13 20:48:27 +0100

    refactor: split format_stats into format_time_stats and format_mem_stats

--- docs/api.md
+++ docs/api.md
@@ -1,66 +1,79 @@
-# LuaMark
+# luamark
 
 ## calculate_stats
 
 ```lua
-function benchmark.calculate_stats(samples: table)
+function luamark.calculate_stats(samples: table)
   -> stats: table
 ```
 
-Calculates statistical metrics from benchmark samples.
+Calculates statistical metrics from timeit or memit samples..
 Includes count, min, max, mean, and standard deviation.
 
 @_param_ `samples` — The table of raw values from timeit or memit.
 
 @_return_ `stats` — A table containing statistical metrics.
 
-## format_stats
+## format_mem_stats
 
 ```lua
-function benchmark.format_stats(stats: table, unit: string)
-  -> txt: string
+function luamark.format_mem_stats(stats: table)
+  -> A: string
 ```
 
-Formats the statistical metrics into a readable string.
+Formats the memory statistics into a readable string.
 
-@_param_ `stats` — The statistical metrics to format.
+@_param_ `stats` — The statistical metrics to format, specifically for memory measurements.
 
-@_param_ `unit` — The unit of measurement for the metrics.
+@_return_ `A` — formatted string representing the memory statistical metrics in kilobytes.
 
-@_return_ `txt` — A formatted string representing the statistical metrics.
+## format_time_stats
+
+```lua
+function luamark.format_time_stats(stats: table)
+  -> A: string
+```
+
+Formats the time statistics into a readable string.
+
+@_param_ `stats` — The statistical metrics to format, specifically for time measurements.
+
+@_return_ `A` — formatted string representing the time statistical metrics in seconds.
 
 ## memit
 
 ```lua
-function benchmark.memit(func: function, runs: number, warmup_runs: number)
+function luamark.memit(func: function, runs: number, warmup_runs: number)
   -> samples: table
 ```
 
 Benchmarks a function for memory usage.
+The memory usage is represented in kilobytes.
 
-@_param_ `func` — The function to benchmark.
+@_param_ `func` — The function to luamark.
 
-@_param_ `runs` — The number of times to run the benchmark.
+@_param_ `runs` — The number of times to run the luamark.
 
-@_param_ `warmup_runs` — The number of warm-up iterations before the benchmark.
+@_param_ `warmup_runs` — The number of warm-up iterations before the luamark.
 
 @_return_ `samples` — A table of memory usage measurements for each run.
 
 ## timeit
 
 ```lua
-function benchmark.timeit(func: function, runs: number, warmup_runs: number, disable_gc: boolean)
+function luamark.timeit(func: function, runs: number, warmup_runs: number, disable_gc: boolean)
   -> samples: table
 ```
 
 Benchmarks a function for execution time.
+The time is represented in seconds.
 
-@_param_ `func` — The function to benchmark.
+@_param_ `func` — The function to luamark.
 
-@_param_ `runs` — The number of times to run the benchmark.
+@_param_ `runs` — The number of times to run the luamark.
 
-@_param_ `warmup_runs` — The number of warm-up iterations before the benchmark.
+@_param_ `warmup_runs` — The number of warm-up iterations before the luamark.
 
-@_param_ `disable_gc` — Whether to disable garbage collection during the benchmark.
+@_param_ `disable_gc` — Whether to disable garbage collection during the luamark.
 
 @_return_ `samples` — A table of time measurements for each run.
--- src/luamark.lua
+++ src/luamark.lua
@@ -1,11 +1,11 @@
 local socket = require("socket")
 local now = socket.gettime
 
----@class M
-local M = {}
+---@class luamark
+local luamark = {}
 
 --- Performs a warm-up for a given function by running it a specified number of times.
---- This helps in preparing the system for the actual M.
+--- This helps in preparing the system for the actual luamark.
 ---@param func function The function to warm up.
 ---@param runs number The number of times to run the function.
 local function warmup(func, runs)
@@ -37,14 +37,14 @@ local function measure_memory(func)
    return memory_used
 end
 
---- Runs a M on a function using a specified measurement method.
+--- Runs a benchmark on a function using a specified measurement method.
 --- Collects and returns raw values from multiple runs of the function.
----@param func function The function to M.
+---@param func function The function to luamark.
 ---@param measure function The measurement function to use (e.g., measure_time or measure_memory).
----@param runs number The number of times to run the M.
----@param warmup_runs number The number of warm-up iterations performed before the actual M.
----@param disable_gc boolean Whether to disable garbage collection during the M.
----@return table samples A table of raw values from each run of the M.
+---@param runs number The number of times to run the luamark.
+---@param warmup_runs number The number of warm-up iterations performed before the actual luamark.
+---@param disable_gc boolean Whether to disable garbage collection during the luamark.
+---@return table samples A table of raw values from each run of the luamark.
 local function run_benchmark(func, measure, runs, warmup_runs, disable_gc)
    warmup_runs = warmup_runs or 10
    warmup(func, warmup_runs)
@@ -69,34 +69,36 @@ local function run_benchmark(func, measure, runs, warmup_runs, disable_gc)
 end
 
 --- Benchmarks a function for execution time.
----@param func function The function to M.
----@param runs number The number of times to run the M.
----@param warmup_runs number The number of warm-up iterations before the M.
----@param disable_gc boolean Whether to disable garbage collection during the M.
+--- The time is represented in seconds.
+---@param func function The function to luamark.
+---@param runs number The number of times to run the luamark.
+---@param warmup_runs number The number of warm-up iterations before the luamark.
+---@param disable_gc boolean Whether to disable garbage collection during the luamark.
 ---@return table samples A table of time measurements for each run.
-function M.timeit(func, runs, warmup_runs, disable_gc)
+function luamark.timeit(func, runs, warmup_runs, disable_gc)
    return run_benchmark(func, measure_time, runs, warmup_runs, disable_gc)
 end
 
 --- Benchmarks a function for memory usage.
----@param func function The function to M.
----@param runs number The number of times to run the M.
----@param warmup_runs number The number of warm-up iterations before the M.
+--- The memory usage is represented in kilobytes.
+---@param func function The function to luamark.
+---@param runs number The number of times to run the luamark.
+---@param warmup_runs number The number of warm-up iterations before the luamark.
 ---@return table samples A table of memory usage measurements for each run.
-function M.memit(func, runs, warmup_runs)
+function luamark.memit(func, runs, warmup_runs)
    return run_benchmark(func, measure_memory, runs, warmup_runs, false)
 end
 
---- Calculates statistical metrics from M samples.
+--- Calculates statistical metrics from timeit or memit samples..
 --- Includes count, min, max, mean, and standard deviation.
 ---@param samples table The table of raw values from timeit or memit.
 ---@return table stats A table containing statistical metrics.
-function M.calculate_stats(samples)
+function luamark.calculate_stats(samples)
    local stats = {}
 
    stats.count = #samples
 
-   local min, max, total = 10000, 0, 0
+   local min, max, total = 2 ^ 1023, 0, 0
    for i = 1, stats.count do
       local sample = samples[i]
       total = total + sample
@@ -121,7 +123,7 @@ end
 ---@param stats table The statistical metrics to format.
 ---@param unit string The unit of measurement for the metrics.
 ---@return string txt A formatted string representing the statistical metrics.
-function M.format_stats(stats, unit)
+function format_stats(stats, unit)
    return string.format(
       "%.8f %s ± %.8f %s per run (mean ± std. dev. of %d runs)",
       stats.mean,
@@ -132,4 +134,18 @@ function M.format_stats(stats, unit)
    )
 end
 
-return M
+--- Formats the time statistics into a readable string.
+---@param stats table The statistical metrics to format, specifically for time measurements.
+---@return string A formatted string representing the time statistical metrics in seconds.
+function luamark.format_time_stats(stats)
+   return format_stats(stats, "s")
+end
+
+--- Formats the memory statistics into a readable string.
+---@param stats table The statistical metrics to format, specifically for memory measurements.
+---@return string A formatted string representing the memory statistical metrics in kilobytes.
+function luamark.format_mem_stats(stats)
+   return format_stats(stats, "kb")
+end
+
+return luamark
